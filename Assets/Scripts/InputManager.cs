112c36a7b9208b999256dbc65457092b9933d2d1
DHawk34 <neobuild@mail.ru>
2024-10-11 18:25:17 +0300

    Initial commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public static class InputManager
+{
+	public static PlayerControls Input { get; } = new PlayerControls();
+	public static event Action<InputActionMap> OnActionMapChanged;
+
+	//public static BindingList<InputActionMap> AlwaysActiveMaps { get; } = new BindingList<InputActionMap>();
+
+
+
+	/// <summary>
+	/// Включает нужную мапу, отключая остальные (кроме <paramref name="Debug"/>). В аргументы посылаем <see cref="InputManager.Input"/>.НужнаяМапа.
+	/// </summary>
+	public static void ToggleActionMap(InputActionMap actionMap)
+	{
+		if (actionMap.enabled)
+			return;
+
+		Input.Disable(); // Отключить все мапы
+		actionMap.Enable();
+		Input.Debug.Enable();
+
+		OnActionMapChanged?.Invoke(actionMap);
+	}
+
+	/// <summary>
+	/// Отключает все мапы, кроме <paramref name="Debug"/>.
+	/// </summary>
+	public static void DisableAllActionMapsExceptDebug()
+	{
+		Input.Disable();
+		Input.Debug.Enable();
+	}
+
+	public static void ToggleMovementInput(bool enabled)
+	{
+		if (enabled)
+			Input.Player.Move.Enable();
+		else
+			Input.Player.Move.Disable();
+	}
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary> Главный класс игрока. Хранит ссылки на многие скрипты и классы.</summary>
+public class Player : MonoBehaviour
+{
+	public PlayerMovement Movement { get; private set; }
+	//public PlayerStats Stats { get; private set; }
+	//public PlayerHealth PlayerHealth { get; private set; }
+	//public PlayerInteractable PlayerInteractable { get; private set; }
+	//public Inventory Inventory { get; private set; }
+
+	//public StateManager StateManager { get; } = new StateManager();
+
+    public SpriteRenderer SpriteRenderer { get; private set; }
+
+	public bool IsUsingGamepad { get; private set; }
+
+
+
+	// Input
+	private PlayerControls input;
+	private PlayerInput playerInput;
+
+
+
+	// ==================================================
+	// Awake
+	// ==================================================
+	void Awake()
+	{
+		// Input system
+		input = InputManager.Input;
+		playerInput = GetComponent<PlayerInput>();
+		playerInput.onControlsChanged += PlayerInput_onControlsChanged;
+
+		// Components
+		Movement = GetComponent<PlayerMovement>();
+		SpriteRenderer = GetComponent<SpriteRenderer>();
+		//PlayerHealth = GetComponent<PlayerHealth>();
+		//PlayerInteractable = GetComponent<PlayerInteractable>();
+
+		// 
+		//Stats = new PlayerStats();
+		//Inventory = new Inventory(this);
+	}
+
+
+
+	void OnEnable()
+	{
+		input.Player.Enable();
+	}
+	void OnDisable()
+	{
+		input.Player.Disable();
+	}
+
+	private void PlayerInput_onControlsChanged(PlayerInput obj)
+	{
+		IsUsingGamepad = obj.currentControlScheme == input.GamepadScheme.name;
+		
+		// TODO: Хз насчет .Reset()
+		input.Player.AbilityCrosshair.Reset();
+
+		Debug.LogWarning($"Controlls changed {obj.currentControlScheme}!");
+	}
+}
