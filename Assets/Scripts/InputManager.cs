a4e94b96eeb1fccd7d4d7864d0b31a9a85c3950c
CompleXss <mp.maksim30@gmail.com>
2024-10-13 02:59:27 +0300

    fix input manager

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,34 +7,39 @@ using UnityEngine.InputSystem;
 
 public static class InputManager
 {
-	public static PlayerControls Input { get; } = new PlayerControls();
-	public static event Action<InputActionMap> OnActionMapChanged;
+    public static PlayerControls Input { get; } = new PlayerControls();
+    public static event Action<InputActionMap> OnActionMapChanged;
 
-	//public static BindingList<InputActionMap> AlwaysActiveMaps { get; } = new BindingList<InputActionMap>();
+    //public static BindingList<InputActionMap> AlwaysActiveMaps { get; } = new BindingList<InputActionMap>();
+
+    static InputManager()
+    {
+        Input.Debug.Enable();
+    }
 
 
 
-	/// <summary>
-	/// Включает нужную мапу, отключая остальные (кроме <paramref name="Debug"/>). В аргументы посылаем <see cref="InputManager.Input"/>.НужнаяМапа.
-	/// </summary>
-	public static void ToggleActionMap(InputActionMap actionMap)
-	{
-		if (actionMap.enabled)
-			return;
+    /// <summary>
+    /// Включает нужную мапу, отключая остальные (кроме <paramref name="Debug"/>). В аргументы посылаем <see cref="InputManager.Input"/>.НужнаяМапа.
+    /// </summary>
+    public static void ToggleActionMap(InputActionMap actionMap)
+    {
+        if (actionMap.enabled)
+            return;
 
-		Input.Disable(); // Отключить все мапы
-		actionMap.Enable();
-		Input.Debug.Enable();
+        Input.Disable(); // Отключить все мапы
+        actionMap.Enable();
+        Input.Debug.Enable();
 
-		OnActionMapChanged?.Invoke(actionMap);
-	}
+        OnActionMapChanged?.Invoke(actionMap);
+    }
 
-	/// <summary>
-	/// Отключает все мапы, кроме <paramref name="Debug"/>.
-	/// </summary>
-	public static void DisableAllActionMapsExceptDebug()
-	{
-		Input.Disable();
-		Input.Debug.Enable();
-	}
+    /// <summary>
+    /// Отключает все мапы, кроме <paramref name="Debug"/>.
+    /// </summary>
+    public static void DisableAllActionMapsExceptDebug()
+    {
+        Input.Disable();
+        Input.Debug.Enable();
+    }
 }
