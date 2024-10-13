ebddf17d51f4953ec938a833aecf3de761113833
Zelimkhan <nic-gamster@yandex.ru>
2024-10-13 03:42:00 +0300

    fuse pazzle

--- Assets/Scripts/Fuze/Fuse.cs
+++ Assets/Scripts/Fuze/Fuse.cs
@@ -6,17 +6,28 @@ using UnityEngine;
 public class Fuse : MonoBehaviour
 {
     [SerializeField]
-    private InteractableGetItemFromInventory interactable1;
+    private InteractableGetItemFromInventory slot1;
+
     [SerializeField]
-    private InteractableGetItemFromInventory interactable2;
+    private InteractableGetItemFromInventory slot2;
+
     [SerializeField]
-    private InteractableGetItemFromInventory interactable3;
+    private InteractableGetItemFromInventory slot3;
     [SerializeField]
-    private InteractableGetItemFromInventory interactable4;
+    private InteractableGetItemFromInventory slot4;
 
     private int[] winningHand = new int[] {2, 1, 3, 4}; //B A C D
     private int[] currentHand = new int[] {0, 0, 0, 0};
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        slot1.actionInt.AddListener(SetFuse1);
+        slot2.actionInt.AddListener(SetFuse2);
+        slot3.actionInt.AddListener(SetFuse3);
+        slot4.actionInt.AddListener(SetFuse4);
+
+    }
 
     public void SetFuse1(int id)
     {
@@ -49,6 +60,12 @@ public class Fuse : MonoBehaviour
         {
             Debug.Log("Winner");
         }
+
+        Debug.Log(currentHand[0]);
+        Debug.Log(currentHand[1]);
+        Debug.Log(currentHand[2]);
+        Debug.Log(currentHand[3]);
+
 
     }
 
--- Assets/Scripts/Inventory/InteractableGetItemFromInventory.cs
+++ Assets/Scripts/Inventory/InteractableGetItemFromInventory.cs
@@ -4,14 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Localization.SmartFormat.PersistentVariables;
 
 public class InteractableGetItemFromInventory : MonoBehaviour
 {
     [SerializeField] private string[] allowedItemName;
-    [SerializeField] private UnityEvent action;
-    [SerializeField] private UnityEvent<int> actionInt;
+    public UnityEvent action;
+    public UnityEvent<int> actionInt;
 
     private Inventory playerInventory;
+
+    private InventoryItem item;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     void Awake()
     {
@@ -19,15 +23,42 @@ public class InteractableGetItemFromInventory : MonoBehaviour
     }
 
 
-
     void OnMouseDown()
     {
-        if (!playerInventory.TryGetActiveItem(out var item))
-            return;
+        if (item == null)
+        {
+            if (!playerInventory.TryGetActiveItem(out var item))
+                return;
 
-        if (!allowedItemName.Contains(item.name))
-            return;
+            Debug.Log(item.ItemName);
 
-        actionInt?.Invoke((int)item.name[0]);
+            if (!allowedItemName.Contains(item.ItemName))
+                return;
+
+            if (!playerInventory.TryRemoveItem(item))
+                return;
+
+            this.item = item; 
+
+            changeSprite();
+
+            actionInt?.Invoke(item.ItemName[5] - '0');
+
+        }
+        else
+        {
+            if (!playerInventory.TryAddItem(item))
+                return;
+
+            item = null;
+
+            changeSprite();
+
+        }
+    }
+
+    void changeSprite()
+    {
+        spriteRenderer.sprite = item.Sprite;
     }
 }
